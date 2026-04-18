6db11eff16fdfa8a7a8bdf87366746a808e16d58
arkadysmith <nw2025@protonmail.com>
2026-04-18 15:16:07 +0000

    Add files via upload

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,6 +1,10 @@
 waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
 swaybg -i ~/dot_files/.config/wallpaper.jpg >/dev/null 2>&1 &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
-/usr/lib/xdg-desktop-portal-wlr &
+pipewire &
+pipewire-pulse &
+wireplumber &
+pgrep -x xdg-desktop-portal >/dev/null || xdg-desktop-portal &
+/usr/lib/xdg-desktop-portal-wlr 2>&1 &
 pactl set-sink-volume @DEFAULT_SINK@ 100% &
 bash cifs.sh &
